5a8ce635a3a8ed25726f3cf22380a1968e92016d
Nikolay Kolev <kolev@2create.bg>
2024-11-07 16:20:52 +0200

    Update page content

--- app/page.tsx
+++ app/page.tsx
@@ -53,9 +53,11 @@ function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
-      </div>
+      {coverImage &&
+        <div className="mb-8 md:mb-16">
+          <CoverImage title={title} slug={slug} url={coverImage.url} />
+        </div>
+      }
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
@@ -63,9 +65,11 @@ function HeroPost({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <Date dateString={date} />
-          </div>
+          {date &&
+            <div className="mb-4 md:mb-0 text-lg">
+              <Date dateString={date} />
+            </div>
+          }
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
